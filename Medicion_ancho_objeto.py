8a587c54180e2715b5575626cb05814ab205dca0
carlosjaravas <carlosjaravas@gmail.com>
2022-03-28 18:39:58 -0600

    Multiples cambios
    Eliminacion foto amarilla, ajustes de distancias, notebook con evidencias de las pruebas y funcionamiento de la app, se junto el codigo de la notebook en un .py

--- Medicion_ancho_objeto.py
+++ Medicion_ancho_objeto.py
@@ -1,20 +1,7 @@
 #Importar las librerías por utilizar
 import os
 from skimage import io
-from skimage.filters import threshold_otsu
-
-#Obtener la direccion de las imagenes
-path = os.getcwd()
-IMG_ROSA= io.imread(path +r'\IMG_ROSA.jpeg',True)
-IMG_AZUL= io.imread(path +r'\IMG_AZUL.jpeg',True)
-IMG_AMARILLA= io.imread(path +r'\IMG_AMARILLA.jpeg',True)
-IMG_VERDE= io.imread(path +r'\IMG_VERDE.jpeg',True)
-
-#Tuplas de datos para la Aplicación
-Data_Rosa = (IMG_ROSA,128)
-Data_Azul = (IMG_AZUL,157)
-Data_Amarilla = (IMG_AMARILLA,120)
-Data_Verde = (IMG_VERDE,136)
+from skimage.filters import threshold_otsu,sobel
 
 #Binarización de las Imagenes
 def binarize(image,thresh=0):
@@ -51,5 +38,47 @@ def dist_pxl(image):
     else:
         return 0
 
+#Conversión de distancia en pixeles a distancia real
+def size_obj(image,work_dist):
+    sensor_size_h = 4.2
+    focal_dist = 3.31
+    pxl_dist = dist_pxl(image)
+    sens_res_h = sensor_size_h/320
+    dist_sens = pxl_dist*sens_res_h
+    width = work_dist*dist_sens/focal_dist
+    return round(width,1)
 
+#Medicion_ancho_objeto
+def medir_ancho_objeto(Data):
+    work_dist = Data[1]
+    image = Data[0]
+    img_sobel = sobel(image)
+    img_bin = binarize(img_sobel)
+    width = size_obj(img_bin,work_dist)
+    return width
 
+def main():
+    #Obtener la direccion de las imagenes
+    path = os.getcwd()
+    IMG_ROSA= io.imread(path +r'\IMG_ROSA.jpeg',True)
+    IMG_AZUL= io.imread(path +r'\IMG_AZUL.jpeg',True)
+    IMG_VERDE= io.imread(path +r'\IMG_VERDE.jpeg',True)
+    #Tuplas de datos para la Aplicación
+    Data_Rosa = (IMG_ROSA,119)
+    Data_Azul = (IMG_AZUL,139)
+    Data_Verde = (IMG_VERDE,118)
+    #Calculo de los anchos
+    ancho_rosa = medir_ancho_objeto(Data_Rosa)
+    ancho_azul = medir_ancho_objeto(Data_Azul)
+    ancho_verde = medir_ancho_objeto(Data_Verde)
+    val_real = [52,87,56]
+    error = [round(abs(ancho_rosa-val_real[0])/ancho_rosa*100,2),
+            round(abs(ancho_azul-val_real[1])/ancho_azul*100,2),
+            round(abs(ancho_verde-val_real[2])/ancho_verde*100,2)]
+    print(f"El ancho de las diferentes imagenes es:")
+    print(f"Imagen   Ancho (mm)     Error (%)")
+    print(f"Rosa     {ancho_rosa}           {error[0]}")
+    print(f"Azul     {ancho_azul}           {error[1]}")
+    print(f"Verde    {ancho_verde}           {error[2]}")
+
+if __name__ == "__main__": main()
